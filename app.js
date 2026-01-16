bdd4ed126154148e7511f72beb19adee1a716627
abhisheksingh@aviansoft.in <abhisheksinghaviansoft@gmail.com>
2026-01-16 23:22:56 +0530

    firstssss commit

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
 
-// import express from "express";
-// import cors from "cors";
-// import todoRoutes from "./routes/todo.routes.js";
-
-// const app = express();
-
-// // app.use(cors());
-
-// app.use(cors({
-//   origin: "*"
-// }));
-
-
-// app.use(express.json());
-
-// app.get("/health", (req, res) => res.send("OK"));
-// app.use("/api/todos", todoRoutes);
-
-// export default app;
-
-
 import express from "express";
 import cors from "cors";
 import todoRoutes from "./routes/todo.routes.js";
 
 const app = express();
 
-// 🔥 IMPORTANT
 app.use(cors());
-app.options("*", cors()); // <--- THIS LINE WAS MISSING
+
+// app.use(cors({
+//   origin: "*"
+// }));
+
 
 app.use(express.json());
 
@@ -36,3 +18,21 @@ app.get("/health", (req, res) => res.send("OK"));
 app.use("/api/todos", todoRoutes);
 
 export default app;
+
+
+// import express from "express";
+// import cors from "cors";
+// import todoRoutes from "./routes/todo.routes.js";
+
+// const app = express();
+
+// // 🔥 IMPORTANT
+// app.use(cors());
+// app.options("*", cors()); // <--- THIS LINE WAS MISSING
+
+// app.use(express.json());
+
+// app.get("/health", (req, res) => res.send("OK"));
+// app.use("/api/todos", todoRoutes);
+
+// export default app;
