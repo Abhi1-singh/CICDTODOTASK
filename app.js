606eba36ec56a3e9f497e81ed0418b8fab58d35e
abhisheksingh@aviansoft.in <abhisheksinghaviansoft@gmail.com>
2026-01-16 20:43:15 +0530

    first changes  commit

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";            
 import todoRoutes from "./routes/todo.routes.js";
 
 const app = express();
+
+//  CORS middleware (IMPORTANT)
+app.use(cors({
+  origin: "*",                        
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
 app.use(express.json());
 
 app.get("/health", (req, res) => res.send("OK"));
