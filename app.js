163c0d9fe84ff9daf675255e9acb102444943e52
abhisheksingh@aviansoft.in <abhisheksinghaviansoft@gmail.com>
2026-01-16 23:19:06 +0530

    firstsss commit

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 
+// import express from "express";
+// import cors from "cors";
+// import todoRoutes from "./routes/todo.routes.js";
+
+// const app = express();
+
+// // app.use(cors());
+
+// app.use(cors({
+//   origin: "*"
+// }));
+
+
+// app.use(express.json());
+
+// app.get("/health", (req, res) => res.send("OK"));
+// app.use("/api/todos", todoRoutes);
+
+// export default app;
+
+
 import express from "express";
 import cors from "cors";
 import todoRoutes from "./routes/todo.routes.js";
 
 const app = express();
 
-// app.use(cors());
-
-app.use(cors({
-  origin: "*"
-}));
-
+// 🔥 IMPORTANT
+app.use(cors());
+app.options("*", cors()); // <--- THIS LINE WAS MISSING
 
 app.use(express.json());
 
